c347a95de3bc9ea1e2b4828f61e2e5b06f41cecb
Vaidik-7474 <113718679+Vaidik-7474@users.noreply.github.com>
2022-09-19 15:35:21 +0530

    Add files via upload

---
+++ lab2/lab2_tutorial2.dart
@@ -0,0 +1,43 @@
+void main() {
+//COMMENTING CODE  
+ /*
+
+    //this is a single line comment.
+
+    //this is also a comment
+    //over multiple time
+  
+    /*
+      this 
+      is 
+      a
+      multi
+      line
+      comment
+    */
+
+    ///documentation comment
+    ///at your service
+    /**
+     * this also
+     */
+
+ */ 
+  
+//PRINTING OUTPUT ON CONSOLE
+  
+  // this will show a error.
+  // print('It is lv's cat');
+     print("It is lv's cat");
+
+  print(42);
+  print(2+6);
+  print(22/7);
+  print('Hello, Dart Apprentice reader!');
+  print(x);
+
+  
+
+}
+
+
